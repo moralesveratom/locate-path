afc79c6aee89feba77f3b475304b09d7399125ba
Tomás Morales Vera <moralesveratomas@gmail.com>
2022-12-04 19:51:53 -0300

    locator.py modified
    Cambio estructura de logica

--- src/locate_Path_tmorales/locator.py
+++ src/locate_Path_tmorales/locator.py
@@ -2,43 +2,40 @@ import os
 import sys
 
 
-class LocatorClass:
-    def __init__(self, repo_name):
-        self.__entire_disk = os.getcwd()
-        self.__repo_name = repo_name
-        self.__new_path = ''
+def locator_searcher(repo_name):
+    if not repo_name.isdigit():
+        # vars
+        new_path = ''
+        entire_disk = os.getcwd()
 
-    def locator_searcher(self):
-        if not self.__repo_name.isdigit():
+        # si ejecuto el test desde cualquier posicion de cmd, y si no le paso custom params
+        if not bool(entire_disk.__contains__(repo_name)) and len(sys.argv) == 1:
+            arg_path = sys.argv[0]
+            directories = arg_path.split('\\')
 
-            # si ejecuto el test desde cualquier posicion de cmd, y si no le paso custom params
-            if not bool(self.__entire_disk.__contains__(self.__repo_name)) and len(sys.argv) == 1:
-                arg_path = sys.argv[0]
-                directories = arg_path.split('\\')
+            for directory in directories:
+                if directory == repo_name:
+                    return sys.path.append(new_path[1:])
+                else:
+                    new_path = new_path + '\\' + directory
 
-                for directory in directories:
-                    if directory == self.__repo_name:
-                        return sys.path.append(self.__new_path[1:])
-                    else:
-                        self.__new_path = self.__new_path + '\\' + directory
+        # si digo especifocamente la ruta del repo un nivel arriba
+        elif len(sys.argv) > 2 and sys.argv[1] == '--custom-path':
+            custom_path = sys.argv[2]
+            sys.path.append(custom_path)
 
-            # si digo especifocamente la ruta del repo un nivel arriba
-            elif len(sys.argv) > 2 and sys.argv[1] == '--custom-path':
-                custom_path = sys.argv[2]
-                sys.path.append(custom_path)
+        # si el disco donde estoy parado no tiene el repo ej: C:\automation
+        elif entire_disk[:13] != entire_disk[:3] + repo_name:
+            directories = entire_disk.split('\\')
 
-            # si el disco donde estoy parado no tiene el repo ej: C:\automation
-            elif self.__entire_disk[:13] != self.__entire_disk[:3] + self.__repo_name:
-                directories = self.__entire_disk.split('\\')
+            for directory in directories:
+                if directory == repo_name:
+                    return sys.path.append(new_path[1:])
+                else:
+                    new_path = new_path + '\\' + directory
 
-                for directory in directories:
-                    if directory == self.__repo_name:
-                        return sys.path.append(self.__new_path[1:])
-                    else:
-                        self.__new_path = self.__new_path + '\\' + directory
-
-            # por descarte el repo esta al lado del disco, C:\automation
-            else:
-                return sys.path.append(self.__entire_disk[0:3])
+        # por descarte el repo esta al lado del disco, C:\automation
         else:
-            raise TypeError("The param for LocatorClass.__init__(self, repo_name) must be 'string'")
+            return sys.path.append(entire_disk[0:3])
+    else:
+        raise TypeError("The param for locator_searcher(repo_name) must be 'string'")
